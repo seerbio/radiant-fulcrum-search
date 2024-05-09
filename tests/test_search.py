b46f0477821326f9160a279bef64dee43fe45a51
Seth Just <sjust@seer.bio>
2024-05-09 20:24:16 +0000

    Add stub test file

---
+++ tests/test_search.py
@@ -0,0 +1,11 @@
+"""
+`test_search` -- unit tests for utils in `scry_pythia_search.search`
+"""
+
+
+def test_import():
+    """
+    Stub test that importing the root module is successful.
+    TODO: replace this with more meaningful tests
+    """
+    import scry_pythia_search
