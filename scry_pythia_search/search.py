e07f428708906f34b703af9d4cb31174d4e008d6
Seth Just <sjust@seer.bio>
2024-05-09 20:43:08 +0000

    Stub out search backend

---
+++ scry_pythia_search/search.py
@@ -0,0 +1,37 @@
+from os import (
+    PathLike as _PathLike,
+)
+from typing import (
+    Optional as _Optional,
+    Union as _Union,
+)
+
+from wheely.mammoth import (
+    PsmDataset as _PsmDataset,
+)
+
+
+def run_pythia_search(
+    location: _Union[_PathLike, _Iterable[_PathLike]],
+    executable: _Union[str, _PathLike] = "PythiaDIA",
+    mode: str = "serial",
+    **kwargs,
+) -> _PsmDataset:
+    """
+    Execute Pythia locally to search the given location(s).
+
+    Arguments
+    ---------
+    location: one or more `PathLike` values giving the local
+              path of the mzML or Parquet file(s) to search
+    executable: the path of a Pythia executable. By default,
+                the executable `PythiaDIA` will be located
+                using the current value of `$PATH`.
+    mode: if "serial" each of multiple files will be processed
+          sequentially. If "parallel" all files will be run
+          concurrently; this should provide faster execution
+          provided that sufficient CPU and RAM is available.
+          You _must_ ensure that Pythia is configured to use
+          an appropriate number of threads.
+    """
+    raise NotImplementedError("TODO")
